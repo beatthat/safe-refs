3206841133a1c6d95e54eba0ff2ca3f99db45852
larrykirschner <larrykirschner@gmail.com>
2017-09-20 00:49:01 -0700

    0.0.5

--- test/scripts/cp-test2src.js
+++ test/scripts/cp-test2src.js
@@ -4,4 +4,12 @@ const path = require('path');
 const unitySrcRoot = path.join(__dirname, '..');
 const tgtPkgRoot = path.join(unitySrcRoot, '..');
 
-unpm.syncPlugin2Src(unitySrcRoot, tgtPkgRoot);
+unpm.copyFromUnity2PkgRoot(unitySrcRoot, tgtPkgRoot, (err, info) => {
+    if(err) {
+        console.log('cp-test2src failed with error: %j', err);
+        process.exit(1);
+        return;
+    }
+
+    console.log(`cp-test2src succeeded for package ${info.package.name}`);
+});
--- test/scripts/overwrite-test2src.js
+++ test/scripts/overwrite-test2src.js
@@ -4,4 +4,12 @@ const path = require('path');
 const unitySrcRoot = path.join(__dirname, '..');
 const tgtPkgRoot = path.join(unitySrcRoot, '..');
 
-unpm.syncPlugin2Src(unitySrcRoot, tgtPkgRoot, {overwrite: true});
+unpm.copyFromUnity2PkgRoot(unitySrcRoot, tgtPkgRoot, {overwrite: true}, (err, info) => {
+    if(err) {
+        console.log('overwrite-test2src failed with error: %j', err);
+        process.exit(1);
+        return;
+    }
+
+    console.log(`overwrite-test2src succeeded for package ${info.package.name}`);
+});
